f3e22200a8a3ae70ef70facd3000388aa5c43651
Anne Isabelle Macedo <retpolanne@posteo.net>
2024-05-21 21:12:09 -0300

    Add figment for config handling
    Signed-off-by: Anne Isabelle Macedo <retpolanne@posteo.net>

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,67 @@
 use core::time;
-use env_logger::Env;
-use log::info;
-use std::io::Error;
+use figment::{Figment, providers::{Format, Toml, Json, Env, Serialized}};
+use log::{info, trace};
+use serde::{Deserialize, Serialize};
+use std::error::Error;
 use std::sync::atomic::AtomicBool;
 use std::sync::Arc;
+use std::sync::atomic::Ordering;
 use std::thread;
 use signal_hook::flag;
 use signal_hook::consts::TERM_SIGNALS;
 use signal_hook::iterator::SignalsInfo;
 use signal_hook::iterator::exfiltrator::WithOrigin;
 
-fn main() -> Result<(), Error> {
+#[derive(Debug, Deserialize, Serialize)]
+struct Config {
+    /// Ensure that EBS volumes are deleted on termination
+    ///
+    /// By default, this is true. If you prefer to keep it safe, turn this config to false
+    ensure_ebs_deleted_on_term: bool,
+    /// Detection interval, in seconds
+    ///
+    /// Default: 2 seconds
+    detection_interval: u8,
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        Config {
+            ensure_ebs_deleted_on_term: true,
+            detection_interval: 2
+        }
+    }
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let figment = Figment::from(Serialized::defaults(Config::default()))
+        .merge(Toml::file("ebs-autoscale.toml"))
+        .merge(Env::prefixed("EBS_AUTOSCALE_"))
+        .join(Json::file("ebs-autoscale.json"));
+
+    let config : Config = figment.extract()?;
+
     env_logger::init_from_env(
-        Env::default()
+        env_logger::Env::default()
             .filter_or("LOG_LEVEL", "info")
     );
+    info!("Started ebs autoscaler - config {:?}", config);
+
     let term_now = Arc::new(AtomicBool::new(false));
     for sig in TERM_SIGNALS {
         flag::register_conditional_shutdown(*sig, 1, Arc::clone(&term_now))?;
         flag::register(*sig, Arc::clone(&term_now))?;
     }
-
     let mut signals = SignalsInfo::<WithOrigin>::new(TERM_SIGNALS)?;
 
     thread::spawn(move || {
-       loop {
-           info!("Working");
-           thread::sleep(time::Duration::from_millis(100000));
-       }
+        while !term_now.load(Ordering::Relaxed) {
+           trace!("Checking if autoscaling is needed");
+           thread::sleep(
+               time::Duration::from_secs(config.detection_interval.into())
+           );
+        }
+        trace!("Stopped checking...");
     });
 
     for info in &mut signals {
@@ -39,7 +73,10 @@ fn main() -> Result<(), Error> {
         }
     }
 
-    thread::sleep(time::Duration::from_millis(10000));
+    if config.ensure_ebs_deleted_on_term {
+        info!("Deleting ebs volumes before termination...");
+        thread::sleep(time::Duration::from_millis(10000));
+    }
     info!("Finished ebs autoscaler gracefully");
 
     Ok(())
